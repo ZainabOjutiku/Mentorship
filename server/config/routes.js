c669b9859d5ce851740950c8f80f2f88544fb21c
Zainab Ojutiku <zainabojutiku@Zainabs-MacBook-Pro.local>
2019-04-23 15:57:34 -0700

    testing push

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,31 @@
 var mentors = require('../controllers/mentors');
 
 module.exports = function(app) {
-
+    app.get('/', function (req, res) {
+        mentors.index(req, res);
+    });
+    app.get('/allmentors', function (req, res) {
+        mentors.AllMentors(req,res);
+    });
+    app.post('/newmentor', function (req, res) {
+        mentors.NewMentor(req,res);
+    });
+    app.post('/newRating/:id', function (req, res) {
+        console.log("ratings",req.params.id);
+        mentors.NewRating(req,res);
+    });
+    app.put('/mentor/update/:id', function (req, res) {
+        console.log("routes",req.params.id);
+        mentors.updateMentor(req,res);
+    });
+    app.get('/onementor/:id', function (req, res) {
+        console.log("routes",req.params.id);
+        mentors.findMentor(req,res);
+    });
+    app.delete('/deletementor/:id', function (req, res) {
+        mentors.destroy(req,res);
+    });
+    app.delete('/delete/rating/:id', function (req, res) {
+        mentors.destroyRating(req,res);
+    });
+}
